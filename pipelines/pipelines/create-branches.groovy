a3a4e5d8b1f7e21c5da7324cd31888e8f87e303f
Emma <emmivi@gmail.com>
2017-12-09 16:32:11 +0100

    Modificamos el fichero create-branches.groovy

--- pipelines/pipelines/create-branches.groovy
+++ pipelines/pipelines/create-branches.groovy
@@ -1,44 +1,19 @@
 node() {
 	def mvnHome = tool 'MVN3';
-	def urlArtifact = "${repository}/${artifact}/trunk";
+	def urlArtifact = "${repository}/${artifact}/master";
 	def newUrlBranch = "${repository}/${artifact}/branches/${artifact}-${newBranch}";
 
-	stage("Preparation"){
+	stage('Preparation'){
 		deleteDir();
-		if (branch != "trunk"){
+		if (branch != "master"){
 			urlArtifact = "${repository}/${artifact}/${branch}/${branchName}";
 		}
 
 		print "Check the url of artifact ${urlArtifact}";
-		def out = sh("svn ls ${urlArtifact}");
+		def out = sh("git show HEAD:${urlArtifact}");
 		print "${out}";
 	}
 
-	stage('Checkout code') {
-		print "Export from ${urlArtifact}";
-		sh("svn export ${urlArtifact} ${artifact}");	
-	}
 
-	stage ('Set new version'){
-		print "Set ${newVersion}";
-		def out = sh("${mvnHome}/bin/mvn -f ./${artifact}/pom.xml versions:set -DnewVersion=${newVersion}");
-		print "${out}";
-	}
-
-	stage ('Commit version'){
-		out = sh("${mvnHome}/bin/mvn -f ./${artifact}/pom.xml versions:commit");
-		print "${out}";
-	}
-
-	stage ('Import Branch'){
-		sh("rm -Rf ./target");
-		def out = sh("svn import -m 'import branch' ./${artifact} ${newUrlBranch}");
-		print "${out}";	
-	}
-
-	stage ('Build'){
-		def result = build job: "${buildBranch}", propagate: false, parameters: [[$class: "ListSubversionTagsParameterValue", name: "artifact", tag: "${artifact}", tagsDir: "${repository}"], [$class: "GeneratorKeyValueParameterValue", name: "branch_name", value: "${artifact}-${newBranch}"]]
-		print "${result}";	
-	}
 
 }
